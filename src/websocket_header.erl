6a998149fecb60a9e208e0b2414725148f585d5c
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2011-05-20 11:21:18 -0400

    refatorado expressoes regulares em websocket_header.erl

--- src/websocket_header.erl
+++ src/websocket_header.erl
@@ -14,25 +14,32 @@
 
 -export([parse_request/1]).
 
+-define(RE_HTTP_METHOD, "[A-Z]+").
+-define(RE_HTTP_URI,   "/[^ ]*").
+-define(RE_REQ, "^(" ++ ?RE_HTTP_METHOD ++ ") ("++ ?RE_HTTP_URI ++") HTTP/1.1$").
+
+-define(RE_FIELD_NAME, "[^:]+").
+-define(RE_FIELD_VALUE, "[^$]+").
+-define(RE_FIELD, "^("++ ?RE_FIELD_NAME  ++"): ("++ ?RE_FIELD_VALUE ++")$").
+
 -define(RE_LINES, "[\r\n]+").
 -define(RE_LINES_OPT, [{return, list}]).
-
--define(RE_REQ, "^([A-Z]+) (/[^ ]*) HTTP/1.1$").
--define(RE_FIELD, "^([^: ]+): ([^$]+)$").
 -define(RE_FIELD_OPT, [{capture, all_but_first, list}]).
+-define(RE_REQ_OPT, ?RE_FIELD_OPT).
 %------------------------------------------------------------------------------
-parse_request(WebSocketHeader) when is_binary(WebSocketHeader) ->
-	parse_request(binary_to_list(WebSocketHeader));
-parse_request(WebSocketHeader) ->
-	[WsRequest|LineFields] = 
-		re:split(WebSocketHeader, ?RE_LINES, ?RE_LINES_OPT),
+parse_request(Header) when is_binary(Header) ->
+	parse_request(binary_to_list(Header));
+parse_request(Header) ->
+	[RequestLine|FieldLines] = 
+		re:split(Header, ?RE_LINES, ?RE_LINES_OPT),
 
-	RequestField = map_request(WsRequest),
-	Fields = map_field(LineFields),
-	RequestField ++ Fields.
+	RequestField = map_request(RequestLine),
+	HeaderFields = map_field(FieldLines),
+
+	RequestField ++ HeaderFields.
 %------------------------------------------------------------------------------
 map_request(Line) ->
-	case re:run(Line, ?RE_REQ, ?RE_FIELD_OPT) of 
+	case re:run(Line, ?RE_REQ, ?RE_REQ_OPT) of 
 		{match, [Method, Path]} ->
 			[{method, Method}, {path, Path}];
 		nomatch ->
