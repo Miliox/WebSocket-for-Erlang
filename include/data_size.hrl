4d7bb4da37fcc1c3ac51c4853afb58041cffa552
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2011-05-24 12:02:21 -0400

    refatorado ws_hixie76_lib: dimensoes de dados definidos em data_size.hrl renomeado variaveis ambiguas completude das funcoes expansao da api

---
+++ include/data_size.hrl
@@ -0,0 +1,10 @@
+%% LUDUS - Grupo de Desenvolvimento de Jogos
+%% TCC1: Trabalho de Conclusao de Curso - 1
+%% Aluno : Emiliano Carlos de Moraes Firmino ( elmiliox@gmail.com )
+%% Orientador : Prof Jucimar Jr ( jucimar.jr@gmail.com )
+%% Objetivo : Abstrai as dimensoes dos varios tipos de dados
+%% Criado: 05/24/11 11:13:53 (HPC)
+
+-define(ASCII, 16#7F).
+-define(BYTE,  16#FF).
+-define(INT4, 16#7FFFFFFF).
--- src/ws_hixie76_lib.erl
+++ src/ws_hixie76_lib.erl
@@ -2,7 +2,7 @@
 %% TCC1: Trabalho de Conclusao de Curso - 1
 %% Aluno: Emiliano Carlos de Moraes Firmino
 %% Orientador : Prof Jucimar Jr ( jucimar.jr@gmail.com )
-%% Objetivo : <OBJETIVO>
+%% Objetivo : Modulo que trata de aspectos especifico do Hixie76
 %% Criado: 05/23/11 13:29:53 (HPC)
 
 %% @author Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
@@ -13,20 +13,21 @@
 -vsn(1).
 
 -include("ws_protocol_header.hrl").
-
--export([gen_response/1, make_trial/0]).
--define(ASCII, 16#7F).
--define(BYTE,  16#FF).
--define(KEY3_SIZE, 8).
+-include("data_size.hrl").
+-export([gen_response/1, make_trial/0, resolve_trial/1]).
+-export([encode_key/2, decode_key/1]).
+-export([random_encode_key/0, random_key3/0]).
+%------------------------------------------------------------------------------
+-define(SPACE, $ ).
+-define(NO_SPACE,  0).
 -define(MIN_SPACE, 1).
 -define(MAX_SPACE, 16).
--define(INT4, 16#7FFFFFFF).
--define(NOSPACE, 0).
-
--define(VALID_CHAR, 
+%------------------------------------------------------------------------------
+-define(KEY3_SIZE, 8).
+-define(VALID_PAD_CHAR, 
 	"abcdefghijklmnopqrstuvwxyz" ++
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZ" ++
-	"'\"\\{}[]?.,<>|+-*%!@&^$#").
+	"'\"\\{}[]?.,<>|+-*%!@&^$#:;_").
 %------------------------------------------------------------------------------
 gen_response(Request) ->
 	case catch(gen_response_1(Request)) of
@@ -73,18 +74,33 @@ resolve_trial(Request) when is_list(Request)->
 	{undefined, K3} = lists:last(Request),
 	
 	resolve_trial({K1, K2, K3});
-resolve_trial({EncKey1, EncKey2, StrKey3}) ->
+resolve_trial({EncKey1, EncKey2, StrKey3}) 
+when 
+	is_list(EncKey1) andalso
+	is_list(EncKey2) andalso
+	is_list(StrKey3) ->
+
 	Key1 = decode_key(EncKey1),
 	Key2 = decode_key(EncKey2),
 	Key3 = list_to_binary(StrKey3),
 
+	resolve_trial({Key1, Key2, Key3});
+resolve_trial({Key1, Key2, Key3})
+when 
+	is_integer(Key1) andalso 
+	is_integer(Key2) andalso
+	is_binary(Key3)  andalso
+	size(Key3) == ?KEY3_SIZE ->
+
 	binary_to_list(
 		erlang:md5(
-			<<Key1:32, Key2:32, Key3/binary>>)).
+			<<Key1:32, Key2:32, Key3/binary>>));
+resolve_trial(_) ->
+	erlang:error(badarg).
 %------------------------------------------------------------------------------
 decode_key(K) ->
-	{Number, []} = string:to_integer([I|| I <- K, I >= $0, I =< $9]),
-	TotalSpaces  = length([Sp|| Sp <- K, Sp == $  ]),
+	{Number, []} = string:to_integer([I|| I <- K, I >= $0 andalso I =< $9]),
+	TotalSpaces  = length([Sp|| Sp <- K, Sp == ?SPACE ]),
 
 	trunc(Number / TotalSpaces).
 %------------------------------------------------------------------------------
@@ -109,47 +125,52 @@ random_encode_key() ->
 			random_encode_key()
 	end.
 %------------------------------------------------------------------------------
-encode_key(Key, Spaces) when is_integer(Key) andalso Spaces > 0 ->
-	EncKey = Key * Spaces,
+encode_key(Key, Spaces) 
+when 
+	is_integer(Key) andalso 
+	Spaces >= ?MIN_SPACE ->
 
+	EncKey     = Key * Spaces,
 	[Int|Rest] = integer_to_list(EncKey),
-	Reverse = lists:reverse(Rest),
+	Reverse    = lists:reverse(Rest),
 
-	[Int|encode_key_1(Reverse, Spaces, [])].
+	[Int|encode_key_1(Reverse, Spaces, [])];
+encode_key(_, _) ->
+	erlang:error(badarg).
 %------------------------------------------------------------------------------
-encode_key_1([], ?NOSPACE, Buffer) ->
+encode_key_1([], ?NO_SPACE, Buffer) ->
 	lists:reverse(Buffer);
-encode_key_1([Int|Rest]=Number, ?NOSPACE, Buffer) ->
+encode_key_1([Int|Rest]=Number, ?NO_SPACE, Buffer) ->
 	case random:uniform() of
-		_Trash when _Trash < 0.5 ->
-			encode_key_1(Number, ?NOSPACE, [random_char()|Buffer]);
+		_Pad when _Pad < 0.5 ->
+			encode_key_1(Number, ?NO_SPACE, [pad_char()|Buffer]);
 		_Int ->
-			encode_key_1(Rest, ?NOSPACE, [Int|Buffer])
+			encode_key_1(Rest,   ?NO_SPACE, [Int|Buffer])
 	end;
 encode_key_1([], Spaces, Buffer) ->
 	case random:uniform() of
-		_Trash when _Trash < 0.5 ->
-			encode_key_1([], Spaces, [random_char()|Buffer]);
+		_Pad when _Pad < 0.5 ->
+			encode_key_1([], Spaces,     [pad_char()|Buffer]);
 		_Space ->
-			encode_key_1([], (Spaces-1), [$ |Buffer])
+			encode_key_1([], (Spaces-1), [?SPACE|Buffer])
 	end;
 encode_key_1([Int|Rest]=Number, Spaces, Buffer) ->
 	case random:uniform() of
-		_Trash when _Trash < 0.33 ->
-			encode_key_1(Number, Spaces, [random_char()|Buffer]);
+		_Pad when _Pad < 0.33 ->
+			encode_key_1(Number, Spaces,     [pad_char()|Buffer]);
 		_Space when _Space < 0.66 ->
-			encode_key_1(Number, (Spaces-1), [$ |Buffer]);
+			encode_key_1(Number, (Spaces-1), [?SPACE|Buffer]);
 		_Int ->
-			encode_key_1(Rest, Spaces, [Int|Buffer])
+			encode_key_1(Rest,   Spaces,     [Int|Buffer])
 	end.
 %------------------------------------------------------------------------------
-random_char() ->
+pad_char() ->
 	Char = random:uniform(?ASCII),
-	case lists:member(Char, ?VALID_CHAR) of
+	case lists:member(Char, ?VALID_PAD_CHAR) of
 		true ->
 			Char;
 		false ->
-			random_char()
+			pad_char()
 	end.
 %------------------------------------------------------------------------------
 random_key3() ->
