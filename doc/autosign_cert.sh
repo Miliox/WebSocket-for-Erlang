eb03cc68bd3ab1a0fdb9cc72c97c76aea9c75cb5
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2011-07-02 14:02:59 -0400

    refatorado geracao de certificado autoassinado

--- doc/autosign_cert.sh
+++ doc/autosign_cert.sh
@@ -2,6 +2,21 @@
 # author: Emiliano Carlos de Moraes Firmino 
 # criado em 06/28/11 23:13:53 (HPC)
 # descricao: http://pdincau.wordpress.com/2011/06/22/a-brief-introduction-to-ssl-with-erlang/
-openssl genrsa -out key.pem 1024
-openssl req -new -key key.pem -out request.pem
-openssl x509 -req -days 7 -in request.pem -signkey key.pem -out certificate.pem
+Ext=pem
+BitLen=1024
+ExpireDays=7
+
+KeyFile=key.${Ext}
+ReqFile=request.${Ext}
+CertFile=certificate.${Ext}
+
+openssl genrsa \
+	-out ${KeyFile} ${BitLen}
+openssl req -new \
+	-key ${KeyFile} \
+	-out ${ReqFile}
+openssl x509 -req \
+	-days ${ExpireDays} \
+	-in ${ReqFile} \
+	-signkey ${KeyFile} \
+	-out ${CertFile}
